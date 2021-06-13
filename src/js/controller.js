5cf635c85ee65fd953740daf914f042da100cf04
Denver-Meadows <meadows8200@gmail.com>
2021-06-13 18:27:50 -0400

    Added update method to View that renders any changed data by updating servings

--- src/js/controller.js
+++ src/js/controller.js
@@ -79,7 +79,7 @@ const controlPagination = function(goToPage) {
  */
 const controlServings = function(newServings) {
   model.updateServings(newServings);
-  recipeView.render(model.state.recipe);
+  recipeView.update(model.state.recipe)
 };
 
 
--- src/js/views/View.js
+++ src/js/views/View.js
@@ -24,6 +24,41 @@ export default class View {
     this._parentElement.insertAdjacentHTML('afterbegin', markup)
   };
 
+  /**
+   * Will compare changes to the DOM and update only those elements that were changed.
+   * @param {object} data 
+   * @summary Takes in the updated data from the state, generates a newMarkup to compare to the current markup and changes as needed.  Not the best algorithm but works for this small app.
+   */
+  update = function(data) {
+    if (!data || data.length === 0) return this.renderError();
+
+    this._data = data
+    const newMarkup = this._generateMarkup();
+
+    // 1) Converting the string returned by newMarkup to a new DOM object that lives in the memory, not on the page.
+    const newDOM = document.createRange().createContextualFragment(newMarkup);
+
+    // 2) Selecting all elements from the newDOM which returns a nodeList, put them in an array so they can be compared to the current DOM elements.
+    // const newElements = newDOM.querySelectorAll('*');
+    const newElements = Array.from(newDOM.querySelectorAll('*'));
+
+    // 3) Put the current DOM elements in array for comparison.
+    const curElements = Array.from(this._parentElement.querySelectorAll('*'));
+
+    // 4) Check for changed Text and updates.  If there isn't a text value, the nodeValue method will return null and the replacement will not take place.
+    newElements.forEach((newEl, i) => {
+      const curEl = curElements[i];
+      if (!newEl.isEqualNode(curEl) && newEl.firstChild?.nodeValue.trim() !== '') {
+        curEl.textContent = newEl.textContent
+      };
+
+      // 5) Check if any attributes have changed and update. 
+      if (!newEl.isEqualNode(curEl)) {
+        Array.from(newEl.attributes).forEach(attr => curEl.setAttribute(attr.name, attr.value));
+      };
+    });
+  };
+
   // Render Spinner in between loading images
   renderSpinner = function() {
     const markup = `
